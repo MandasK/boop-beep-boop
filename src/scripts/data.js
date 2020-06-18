26a7281547bfc7bfe321102fff0c0c1704b2d452
MandasK <king.amanda.r@gmail.com>
2020-06-18 11:16:30 -0500

    added fetch post call

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,23 +1,26 @@
 
 let journalEntries = []
-// const getEntriesData = () => {
-//     return fetch("http://localhost:8088/entries").then(
-//         (httpResponse) => {
-//             return httpResponse.json()
-//         }
-//     )
 
-//     .then(
-//         (arrayOfEntries) => {
-//             journalEntries = arrayOfEntries
-//         }
-//     )
-// }
+const url = "http://localhost:8088";
 
 
 const API = {
     getJournalEntries () {
-        return fetch("http://localhost:8088/entries")
-            .then(response => response.json()).then( (arrayOfEntries) => journalEntries = arrayOfEntries);
+        return fetch(`${url}/entries`)
+            .then(response => response.json());
+    },
+
+    addAJournal (journalObject) {
+        return fetch(`${url}/entries`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(journalObject)
+        }).then(response => response.json())
     }
+    
 }
+
+
+export default API
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -16,3 +16,5 @@
 
 API.getJournalEntries().then(() => renderJournalEntries())
 
+// In your main JavaScript module (journal.js) add a click event listener to the Record Journal Entry button at the bottom of your form. When the user clicks the button, you need to create a new entry in your API. The HTTP method that you use to create resources is POST. Guidance on syntax is provided below.
+
